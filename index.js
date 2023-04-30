2e0f5d0cca7deef0c84813226156b8699e15d57d
Atharva <adrooney322@gmail.com>
2023-04-30 12:26:54 +0100

    removed logs and date checks added with formatting

--- index.js
+++ index.js
@@ -15,32 +15,37 @@ const upload = multer({ storage });
 async function extractDates (imageBuffer) {
     const [result] = await client.textDetection(imageBuffer)
     if (result.error) {
-        console.log(result.error)
+        console.log(`Error in OCR: ${result.error}`)
         throw new Error('Error in OCR', result.error);
     }
     if (result.fullTextAnnotation) {
-        // console.log(result.fullTextAnnotation)
         const textAnnotations = result.fullTextAnnotation.text.split('\n');
         let dob = null;
         let doe = null;
         for (let i = 0; i < textAnnotations.length; i++) {
-            if (textAnnotations[i].match(/date of birth/i) && !dob) {
-                dob = textAnnotations[i + 1].trim()
-            }
-            if ((textAnnotations[i].match(/date of expiry/i) || textAnnotations[i].match(/expiry date/i)) && !doe) {
-                doe = textAnnotations[i + 1].trim()
-            }
-
             if (dob && doe) {
                 break;
             }
+            if ((textAnnotations[i].match(/date of birth/i) || (textAnnotations[i].match(/birth date/i))) && !dob) {
+                dob = new Date(textAnnotations[i + 1].trim())
+            }
+            if ((textAnnotations[i].match(/date of expiry/i) || textAnnotations[i].match(/expiry date/i)) && !doe) {
+                doe = new Date(textAnnotations[i + 1].trim())
+            }
         }
-        console.log("dob", dob, "doe", doe)
+        if (dob && dob > new Date()) {
+            throw new Error('Invalid date of birth');
+        }
         return {
-            birthDate: dob && new Date(dob),
-            expiryDate: doe && new Date(doe)
+            birthDate: dob && dob.toLocaleDateString('en-GB', {
+                day: 'numeric', month: 'short', year: 'numeric'
+            }),
+            expiryDate: doe && doe.toLocaleDateString('en-GB', {
+                day: 'numeric', month: 'short', year: 'numeric'
+            })
         };
     } else {
+        console.log('No text found while getting response from google cloud vision')
         throw new Error('No text found');
     }
 }
@@ -51,12 +56,8 @@ app.post('/extract', upload.single('image'), async (req, res) => {
         const { birthDate, expiryDate } = await extractDates(buffer);
         res.json({ birthDate, expiryDate });
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({ message: 'Internal server error', error: error.message });
     }
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
-
-(() => {
-    console.log(new Date("23 APR 2020"))
-})()
